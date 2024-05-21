d5c542bf748b4feb1f63deb91e0697af93beaeec
Salman Junaed Islam <salman9blog@gmail.com>
2024-05-21 21:09:03 +0600

    Client Basic setup done

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Shared/Footer/Footer";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-center">This is Home page</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
